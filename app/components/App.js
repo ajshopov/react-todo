e7c64f33a68622840e02e2a99b73e54220df6561
ajshopov <alexanderjshopov@gmail.com>
2018-04-04 12:49:14 +1000

    todo list setup

---
+++ app/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PasswordInput from './PasswordInput'
+import ToDo from './ToDo'
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>
+      <ToDo
+        listName="Grocery"
+        todos={
+          ['chips']
+        }
+      />
+    </div>
+  }
+
+}
---
+++ app/components/ToDo.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default class ToDo extends React.Component{
+  constructor(props){
+    super(props)
+    this.addNewItem = this.addNewItem.bind(this)
+    this.state = {
+      todos: props.todos || [],
+      addNewToDo: ""
+    }
+  }
+
+  addNewItem(){ // bind.this so this.state refers to ToDo class, not the button
+    if (this.state.addNewToDo.trim().length > 0) {
+      const newToDos = this.state.todos.concat(this.state.addNewToDo)
+      this.setState({todos: newToDos, addNewToDo: ""})
+    }
+  }
+
+  render(){
+    const todos = this.state.todos
+
+
+
+    return(
+      <div>
+        <h1>My {this.props.listName} List</h1>
+        {
+          todos.length > 0 ? todos.map((item, index) => <p key={index}> {item} </p>) : `No ${this.props.listName} items`
+        }
+        
+
+        <input 
+          type="text"
+          value={this.state.addNewToDo}
+          onChange={(event) => {
+            this.setState({addNewToDo: event.target.value}) 
+            // console.log(event.target.value)
+          }}
+        />
+
+        <button onClick={this.addNewItem}>
+          Add
+        </button>
+      </div>
+    )
+  }
+}
